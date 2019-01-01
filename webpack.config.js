387e419262440ea40b7dcdeb6e6e8653d1739bf2
Ben Allfree <ben@benallfree.com>
2019-01-01 14:16:19 -0800

    CHckpoint

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,39 @@
-var path = require('path')
+const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const os = require('os')
+const ExtraWatchWebpackPlugin = require('extra-watch-webpack-plugin')
+
+let ip = null
+let ifaces = os.networkInterfaces()
+for (let dev in ifaces) {
+  const iface = ifaces[dev].filter(function(details) {
+    return details.family === 'IPv4' && details.internal === false
+  })
+  if (iface.length > 0) ip = iface[0].address
+}
+
+const devServer = {
+  contentBase: [
+    path.join(__dirname, 'www'),
+    path.join(__dirname, 'platforms/ios/www'),
+  ],
+  host: ip,
+  port: 8080,
+  hot: true,
+}
+
+const devServerHost = `http://${devServer.host}:${devServer.port}/`
 
 module.exports = {
   target: 'web',
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'www'),
+    publicPath: devServerHost,
+
     filename: 'app.js',
   },
+  mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
       {
@@ -35,12 +62,25 @@ module.exports = {
       },
     ],
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    compress: true,
-    port: 8080,
-    hot: true,
-    host: '0.0.0.0',
-  },
+  devServer,
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtraWatchWebpackPlugin({
+      dirs: ['../repos/cordova-live-update/dist'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      templateParameters: {
+        host: devServerHost,
+      },
+    }),
+    new webpack.DefinePlugin({
+      LIVE_UPDATE_URL: JSON.stringify(
+        {
+          production: 'https://cordovaliveupdate.com/asdf',
+        }[process.env.NODE_ENV] || `http://${ip}:4000`,
+      ),
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    }),
+  ],
 }
